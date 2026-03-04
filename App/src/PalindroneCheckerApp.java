33b78413b0484f524e204191d9882bd7e1e99ab3
ANKIT <az7420@srmist.edu.in>
2026-03-04 22:48:05 +0530

    [ANKIT] added UC1-WelcomePage

---
+++ App/src/PalindroneCheckerApp.java
@@ -0,0 +1,20 @@
+public class PalindroneCheckerApp {
+    public static void main(String[] args) {
+
+
+        System.out.println("======================================");
+        System.out.println("     Welcome to Palindrome Checker    ");
+        System.out.println("======================================");
+
+        System.out.println("Application Name : Palindrome Checker App");
+        System.out.println("Version          : 1.0");
+        System.out.println("Developer        : Ankitt ");
+
+        System.out.println("--------------------------------------");
+        System.out.println("This application checks whether a");
+        System.out.println("given string is a palindrome or not.");
+        System.out.println("--------------------------------------");
+
+        System.out.println("Application started successfully.");
+    }
+}
